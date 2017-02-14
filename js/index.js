ffa35b8b33a89aa57a5f71fdae95ad04904f9a2e
Xbaal <becker.konrad@gmail.com>
2017-02-14 14:42:13 +0100

    add feature: faces with less than 3 vertices (with normal)

--- js/index.js
+++ js/index.js
@@ -114,30 +114,54 @@ Object.assign(Edge.prototype, {
   hoverMaterial: new THREE.MeshLambertMaterial({ color: 0xcccccc })
 });
 
-function Face(vertices) {
+function Face(vertices, normal) {
   //this class assumes, that the vertices are coplanar
   this.vertices = vertices;
   var geometry = new THREE.Geometry();
   geometry.vertices = vertices.map(function(v) {
     return v.position;
   });
-  var subFace;
-  for (var i = 0; i < vertices.length - 2; i++) {
-    subFace = new THREE.Face3( 0, i + 1, i + 2 );
-    subFace.color = new THREE.Color( 0xffffff );
-    geometry.faces.push(subFace);
+  if (vertices.length < 3) {
+    if (!normal) {
+      throw new Error("Faces with less than 3 vertices must have a normal defined!");
+    } else {
+      if (normal instanceof Array) {
+        normal = new THREE.Vector3(normal[0], normal[1], normal[2]);
+      }
+    }
+
+    if (vertices.length === 2) {
+      var edgeLength = vertices[0].position.distanceTo( vertices[1].position );
+      console.log(edgeLength);
+      geometry = new THREE.PlaneGeometry( 100 + edgeLength + 100, 100 );
+      THREE.Mesh.call(this, geometry, this.planeMaterial);
+      this.position.addVectors( vertices[0].position, vertices[1].position ).multiplyScalar( .5 );
+    } else {
+      geometry = new THREE.PlaneGeometry( 100, 100 );
+      THREE.Mesh.call(this, geometry, this.planeMaterial);
+      this.position.add( vertices[0].position );
+    }
+    this.lookAt(normal);
+  } else {
+    var subFace;
+    for (var i = 0; i < vertices.length - 2; i++) {
+      subFace = new THREE.Face3( 0, i + 1, i + 2 );
+      subFace.color = new THREE.Color(0xffffff);
+      geometry.faces.push(subFace);
+    }
+    geometry.computeFaceNormals();
+    this.a = normal || subFace.normal;
+    this.b = this.a.dot(vertices[0].position);
+    console.log(this.a.x + "*x + " + this.a.y + "*y + " + this.a.z + "*z <= " + this.b);
+    THREE.Mesh.call(this, geometry, this.frontFaceMaterial);
   }
-  geometry.computeFaceNormals();
-  this.a = subFace.normal;
-  this.b = this.a.dot(vertices[0].position);
-  console.log(this.a.x + "*x + " + this.a.y + "*y + " + this.a.z + "*z <= " + this.b);
-  THREE.Mesh.call(this, geometry, this.frontFaceMaterial);
 }
 Face.prototype = Object.create(THREE.Mesh.prototype);
 Face.prototype.constructor = Face;
 Object.assign(Face.prototype, {
   frontFaceMaterial: new THREE.MeshBasicMaterial({ color: 0xffffff, side: THREE.FrontSide, transparent: true, opacity: 0.5 }),
-  backFaceMaterial: new THREE.MeshBasicMaterial({ color: 0xffffff, side: THREE.BackSide, transparent: true, opacity: 0.5 })
+  backFaceMaterial: new THREE.MeshBasicMaterial({ color: 0xffffff, side: THREE.BackSide, transparent: true, opacity: 0.5 }),
+  planeMaterial: new THREE.MeshBasicMaterial({ color: 0xffffff, side: THREE.DoubleSide, transparent: true, opacity: 0.5 })
 });
 
 /*
@@ -152,7 +176,9 @@ scene.add( arrowHelper );
 
 function Polyhedron(data) {
   THREE.Object3D.call(this);
-
+  if (typeof data.normal !== "object") {
+    data.normal = {};
+  }
   var i;
   // convert vertex data to THREE.js vectors
 
@@ -181,12 +207,11 @@ function Polyhedron(data) {
     var v = data.face[i].map(function(index){
       return vertices[index];
     });
-    var face = new Face(v);
+    var face = new Face(v, data.normal[i]);
     faces.push(face);
     this.add(face);
   }
   this.faces = faces;
-  console.log(this.adjacentFaces(this.vertices[0]));
 }
 Polyhedron.prototype = Object.create(THREE.Object3D.prototype);
 Polyhedron.prototype.constructor = Polyhedron;
